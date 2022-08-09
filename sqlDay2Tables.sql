f30f968c241fc7d6616721213ac48ccdb6180871
Ionut Tiplea <ionut.tiplea@bitstone.eu>
2022-08-09 11:46:41 +0300

    created database and tables for streaming

--- sqlDay2Tables.sql
+++ sqlDay2Tables.sql
@@ -1,3 +1,4 @@
+/*creation*/
 CREATE TABLE genre(
 	id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(15),
@@ -35,3 +36,30 @@ CREATE TABLE genre_per_show(
     FOREIGN KEY(show_id) REFERENCES my_show(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(genre_id) REFERENCES genre(id) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = INNODB;
+
+/*populating*/
+
+INSERT INTO genre VALUES
+(NULL, "shonen", "shonen jump, pretty known genre in anime"),
+(NULL, "comedy", "what is comedy? u get the idea :)"),
+(NULL, "drama", "have u seen titanic? u get the idea");
+
+INSERT INTO director VALUES
+(NULL, "dummy1", '1986/12/17', 13),
+(NULL, "dummy2", '1985/12/17', 14),
+(NULL, "dummy3", '1984/12/17', 15),
+(NULL, "dummy4", '1983/12/17', 16);
+
+INSERT INTO streaming_platform VALUES
+(NULL, "animeDao", '.to'),
+(NULL, "Netflix", '.ce vrei'),
+(NULL, "Rito", '.teemo'),
+(NULL, "Poli", '.wtf');
+
+INSERT INTO my_show VALUES
+(NULL, "Naruto", 8.5,'2005/12/17', 1, 5),
+(NULL, "Bleach", 7.8, '2005/12/17', 2, 5),
+(NULL, "Suits", 7.8, '2005/12/17', 3, 6),
+(NULL, "Arcane", 7.8, '2005/12/17', 3, 7),
+(NULL, "Examene", 0.0, '2005/12/17', 4, 8);
+/* insert la genre/show facut din interfata*/
